69e82bc57ffae5872799521a92cb595c173c2d59
Sandoplay <olesgta28@gmail.com>
2025-10-06 21:38:56 +0300

    lab 3 http secured

---
+++ src/main/java/edu/levytskyi/lab1/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+//package edu.levytskyi.lab1.config;
+//
+///*
+// @author Sandoplay
+// @project lab1
+// @class SecurityConfig
+// @version 1.0.0
+// @since 06.10.2025 - 21.21
+//*/
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+////нескінченна війна з конфігурацією spring security але поки най так сидить
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http
+//        .authorizeHttpRequests(auth -> auth
+//            .requestMatchers(HttpMethod.GET, "/api/v1/notes").permitAll()
+//            .anyRequest().authenticated()
+//        )
+//        .httpBasic(Customizer.withDefaults())
+//        .formLogin(Customizer.withDefaults());
+//
+//    return http.build();
+//  }
+//}
